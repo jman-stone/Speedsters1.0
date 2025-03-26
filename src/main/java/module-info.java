cc79587aadc40386dadc6537bb3aa8983f64ac75
jman-stone <jman.stone1@gmail.com>
2025-03-26 16:33:16 -0400

    pushing

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.speedsters1_0 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.speedsters1_0 to javafx.fxml;
+    exports com.example.speedsters1_0;
+}
