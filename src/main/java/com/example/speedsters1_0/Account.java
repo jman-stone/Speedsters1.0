4e81a3689c27f0c7b019defba67543cb76098040
jstone17 <jstone17@dtcc.edu>
2025-04-14 15:09:52 -0400

    sadf

--- src/main/java/com/example/speedsters1_0/Account.java
+++ src/main/java/com/example/speedsters1_0/Account.java
@@ -5,28 +5,56 @@
 
 package com.example.speedsters1_0;
 
+import java.util.ArrayList;
+
 interface AccountInterface {
 	
+	void removeAccount(Account account);
+	ArrayList<Account> getAccountList();
+	void updateEmail(String email);
+	void updatePhoneNum(String phoneNum);
+	void addAccount(Account account);
+	void changePW(String password);
+	void changeUN(String username);
+	void changeName(String name);
+	void deleteAccount();
+	void mergeAccounts();
+
+	String getUsername();
+
+	String getPassword();
+	boolean verifyCredentials(String username, String password);
+}
+
+public class Account implements AccountInterface{
+
 	String Name = "";
 	String Email = "";
 	String UserName = "";
 	String Password = "";
-	int PhoneNum = 999999999;
+	String PhoneNum = "";
 	//Address myAddress = null;
-	
-	String changePW();
-	String changeUN();
-	String changeName();
-	void deleteAccount();
-	void mergeAccounts();
-	
-}
+	ArrayList<Account> AccountList = new ArrayList<>();
 
-public class Account implements AccountInterface{
-	
+
+	@Override
+	public ArrayList<Account> getAccountList() {
+		return AccountList;
+	}
+
 	//Constructor
 	public Account() {
-		
+
+	}
+
+	@Override
+	public void addAccount(Account account) {
+		AccountList.add(account);
+	}
+
+	@Override
+	public void removeAccount(Account account) {
+		AccountList.remove(account);
 	}
 
 	public static void main(String[] args) {
@@ -34,28 +62,60 @@ public class Account implements AccountInterface{
 
 	}
 
-	@Override
-	public String changePW() {
-		// TODO Auto-generated method stub
+
+	public Account getAccountByCredentials(String username, String password) {
+		for (Account account : AccountList) {
+			if (account.getUsername().equals(username) && account.getPassword().equals(password)) {
+				return account;
+			}
+		}
 		return null;
 	}
 
 	@Override
-	public String changeUN() {
+	public boolean verifyCredentials(String username, String password) {
+
+		for (Account accounts : AccountList) {
+			if (accounts.getUsername().equals(username) && accounts.getPassword().equals(password)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	
+
+	@Override
+	public void changePW(String password) {
 		// TODO Auto-generated method stub
-		return null;
+		Password = password;
 	}
 
 	@Override
-	public String changeName() {
+	public void changeUN(String username) {
 		// TODO Auto-generated method stub
-		return null;
+		UserName = username;
 	}
 
+	@Override
+	public void changeName(String name) {
+		// TODO Auto-generated method stub
+		Name = name;
+	}
+
+	@Override
+	public void updateEmail(String email) {
+		Email = email;
+	}
+
+	@Override
+	public void updatePhoneNum(String phoneNum) {
+		PhoneNum = phoneNum;
+	}
 	@Override
 	public void deleteAccount() {
 		// TODO Auto-generated method stub
-		
+		//remove account
 	}
 
 	@Override
@@ -63,5 +123,17 @@ public class Account implements AccountInterface{
 		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public String getUsername() {
+		return UserName;
+	}
+
+	@Override
+	public String getPassword() {
+		return Password;
+	}
+
+	
 
 }
--- src/main/java/com/example/speedsters1_0/HelloController.java
+++ src/main/java/com/example/speedsters1_0/HelloController.java
@@ -1,14 +1,206 @@
 package com.example.speedsters1_0;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.KeyEvent;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+
+
+    public TextField PasswordTextBox;
+    public TextField UserNameTextBox;
+
+    public TextField NewPasswordTextBox;
+    public TextField NewUserNameTextBox;
+    public TextField NameTextBox;
+    public TextField EmailTextBox;
+    public TextField PhoneNumberTextBox;
+    public Button SignInButton;
+    public Button CreateAccountButton;
+    public Label UsersNameLabel;
+    Account baseAccountList = new Account();
+    Account newAccount = new Account();
+    //AccountList accountList = new AccountList();
+    String username;
+    String password;
+    String Name;
+    String Email;
+    String PhoneNumber;
+
+    //ArrayList<Account> AccountList = new ArrayList<>();
+    HelloController controller(){
+
+        PasswordTextBox.setVisible(false);
+        UserNameTextBox.setVisible(false);
+        NewPasswordTextBox.setVisible(false);
+        NewUserNameTextBox.setVisible(false);
+        NameTextBox.setVisible(false);
+        EmailTextBox.setVisible(false);
+        PhoneNumberTextBox.setVisible(false);
+        return this;
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void start() {
+
+
+    }
+
+    //sign in view button
+    @FXML
+    public void onSignInClick(ActionEvent actionEvent) {
+        //Scanner scan = new Scanner(System.in);
+        //reveal username and password fields instead of all this
+        PasswordTextBox.setVisible(true);
+        UserNameTextBox.setVisible(true);
+        SignInButton.setVisible(true);
+        NameTextBox.setVisible(false);
+        EmailTextBox.setVisible(false);
+        PhoneNumberTextBox.setVisible(false);
+        NewPasswordTextBox.setVisible(false);
+        NewUserNameTextBox.setVisible(false);
+        CreateAccountButton.setVisible(false);
+        System.out.println("Please enter your username");
+        //String username = scan.nextLine();
+        System.out.println("Please enter your password");
+        //String password = scan.nextLine();
+
+        //SignInButton.getOnAction();
+//        if(SignInButton.isPressed()&&baseAccountList.verifyCredentials(username, password)){
+//            System.out.println("Sign-in successful! Welcome " + username);
+//
+//        } else {
+//            System.out.println("Invalid username or password.");
+//        }
+
+//        Account blankAccount = new Account();
+//        if (blankAccount.verifyCredentials(username, password)) {
+//            System.out.println("Sign-in successful! Welcome " + username);
+//        } else {
+//            System.out.println("Invalid username or password.");
+//        }
+    }
+
+    @FXML
+    public void onNewUserClick(ActionEvent actionEvent) {
+        PasswordTextBox.setVisible(false);
+        UserNameTextBox.setVisible(false);
+        NameTextBox.setVisible(true);
+        EmailTextBox.setVisible(true);
+        PhoneNumberTextBox.setVisible(true);
+        NewPasswordTextBox.setVisible(true);
+        NewUserNameTextBox.setVisible(true);
+        CreateAccountButton.setVisible(true);
+
+//        if(CreateAccountButton.isPressed()&&baseAccountList.verifyCredentials(username, password)){
+//            System.out.println("Username already exists. Please try again.");
+//        } else {
+//            System.out.println("Account created!");
+//            baseAccountList.addAccount(newAccount);
+//        }
+    }
+    @FXML
+    public void onCartClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void onAmericanCuisineClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void onItalianCuisineClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void onMexicanCuisineClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void onSeafoodCuisineClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void onRestaurantsListClicked(ActionEvent actionEvent) {
+    }
+
+    //I don't think these do anything
+//    @FXML
+//    public void onUserNameEntered(KeyEvent inputMethodEvent) {
+//        username = UserNameTextBox.getText();
+//    }
+//    @FXML
+//    public void onPasswordEntered(InputMethodEvent inputMethodEvent) {
+//        password = PasswordTextBox.getText();
+//    }
+//    @FXML
+//    public void onNameEntered(InputMethodEvent inputMethodEvent) {
+//        Name = NameTextBox.getText();
+//        newAccount.changeName(Name);
+//    }
+//    @FXML
+//    public void onEmailEntered(InputMethodEvent inputMethodEvent) {
+//        Email = EmailTextBox.getText();
+//        newAccount.updateEmail(Email);
+//    }
+//    @FXML
+//    public void onPhoneNumberEntered(InputMethodEvent inputMethodEvent) {
+//        PhoneNumber = PhoneNumberTextBox.getText();
+//        newAccount.updatePhoneNum(PhoneNumber);
+//    }
+//    @FXML
+//    public void onNewPasswordEntered(InputMethodEvent inputMethodEvent) {
+//        password = NewPasswordTextBox.getText();
+//        newAccount.changePW(password);
+//    }
+//    @FXML
+//    public void onNewUserNameEntered(InputMethodEvent inputMethodEvent) {
+//        username = NewUserNameTextBox.getText();
+//        newAccount.changeUN(username);
+//    }
+
+    //These work to create a generic account
+    //Sign in conformation button
+    @FXML
+    public void onSignInInfoEntered(ActionEvent actionEvent) {
+        //username = UserNameTextBox.getText();
+        //password = PasswordTextBox.getText();
+        if(baseAccountList.verifyCredentials(UserNameTextBox.getText(), PasswordTextBox.getText())){
+            System.out.println("Sign-in successful! Welcome " + UserNameTextBox.getText());
+            UsersNameLabel.setText(baseAccountList.getAccountByCredentials(UserNameTextBox.getText(), PasswordTextBox.getText()).Name);
+            PasswordTextBox.setVisible(false);
+            UserNameTextBox.setVisible(false);
+        } else {
+            System.out.println("Invalid username or password.");
+        }
+    }
+
+    //createAccount button
+    @FXML
+    public void onCreateAccountInfoEntered(ActionEvent actionEvent) {
+        newAccount.changeUN(NewUserNameTextBox.getText());
+        newAccount.changePW(NewPasswordTextBox.getText());
+        //username = NewUserNameTextBox.getText();
+        //password = NewPasswordTextBox.getText();
+        newAccount.changeName(NameTextBox.getText());
+        newAccount.updateEmail(EmailTextBox.getText());
+        newAccount.updatePhoneNum(PhoneNumberTextBox.getText());
+        //Name = NameTextBox.getText();
+        //Email = EmailTextBox.getText();
+        //PhoneNumber = PhoneNumberTextBox.getText();
+        if(baseAccountList.verifyCredentials(newAccount.UserName, newAccount.Password)){
+            System.out.println("Username already exists. Please try again.");
+        } else {
+            System.out.println("Account created!");
+            baseAccountList.addAccount(newAccount);
+            System.out.println(baseAccountList.getAccountList().get(0).Name);
+            UsersNameLabel.setText(baseAccountList.getAccountByCredentials(NewUserNameTextBox.getText(), NewPasswordTextBox.getText()).Name);
+            NameTextBox.setVisible(false);
+            EmailTextBox.setVisible(false);
+            PhoneNumberTextBox.setVisible(false);
+            NewPasswordTextBox.setVisible(false);
+            NewUserNameTextBox.setVisible(false);
+            CreateAccountButton.setVisible(false);
+        }
     }
 }
